742c09a8238d6ec6ba34d864a056a6092e125bce
Matthew Morgan <matt@dulcetsoftware.com>
2018-11-19 19:56:30 -0500

    Stub out constructor and solve method

--- maze.js
+++ maze.js
@@ -1,5 +1,12 @@
 class Maze {
 
+  constructor(maze) {
+    this.maze = maze;
+  }
+  solve() {
+    const solution = 'you wish!';
+    return solution;
+  }
 }
 
 module.exports = Maze;
--- maze.spec.js
+++ maze.spec.js
@@ -7,16 +7,16 @@ const Maze = require('./maze');
 const SIMPLE_MAZE = [
   ['S', '0', '1'],
   ['1', '0', '1'],
-  ['1', '0', 'E']
-]
+  ['1', '0', 'E'],
+];
 
 const MODERATE_MAZE = [
   ['S', '0', '0', '1', 'E'],
   ['1', '0', '1', '1', '0'],
   ['1', '0', '0', '1', '0'],
   ['0', '0', '1', '0', '0'],
-  ['1', '0', '0', '0', '0']
-]
+  ['1', '0', '0', '0', '0'],
+];
 
 const LARGE_MAZE = [
   ['S','1','1','0','1','1','0','0','0','1','1','1','1'],
@@ -26,8 +26,8 @@ const LARGE_MAZE = [
   ['1','0','1','0','0','0','0','1','1','1','0','0','1'],
   ['1','0','1','1','1','1','1','1','0','1','1','1','1'],
   ['1','0','0','0','0','0','0','0','0','0','0','0','0'],
-  ['1','1','1','1','1','1','1','1','1','1','1','1','E']
-]
+  ['1','1','1','1','1','1','1','1','1','1','1','1','E'],
+];
 
 describe('Maze business class can', () => {
   test('return an instance of the Maze class', () => {
@@ -35,7 +35,7 @@ describe('Maze business class can', () => {
     expect(maze.constructor.name).toEqual('Maze');
   });
 
-  xtest('skip a test marked with "xtest" instead of "test"', () => {
+  test('skip a test marked with "xtest" instead of "test"', () => {
     expect(null).toEqual(null);
   });
 });
